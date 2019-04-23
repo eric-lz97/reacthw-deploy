174df60e47cb014f0d9b46433b3dd9a79677c5cc
eric lopez <ericlz.25412@gmail.com>
2019-04-23 01:52:29 -0700

    Updates

---
+++ precache-manifest.7cc0ede3495aa29206e8cdb459d86669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63dc2933372f313ef8a1",
+    "url": "/reacthw-deploy/static/js/runtime~main.6b04a01d.js"
+  },
+  {
+    "revision": "1c920c939f79fe0e91c9",
+    "url": "/reacthw-deploy/static/js/main.5a78a3db.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/reacthw-deploy/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1c920c939f79fe0e91c9",
+    "url": "/reacthw-deploy/static/css/main.88f0df87.chunk.css"
+  },
+  {
+    "revision": "60b0037c6db6a3a11e0278641433b225",
+    "url": "/reacthw-deploy/index.html"
+  }
+];
